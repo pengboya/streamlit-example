bc374b97ee81ed57365bdc58ce70281da1b82751
pengboya <148208238+pengboya@users.noreply.github.com>
2023-10-28 20:31:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,11 @@ from streamlit_echarts import st_echarts
 
 if 'sid' not in st.session_state:
     st.session_state.sid = ''
-    st.session_state.chart = None
+    st.session_state.candlestick_options = None
 
 
 def login():
-    response = requests.post('https://www.boyastudio.top/login', json={'email': email, 'password': password})
+    response = requests.post('http://127.0.0.1:5000/login', json={'email': email, 'password': password})
     receive = response.json()
     st.session_state.sid = receive.get('sid')
     st.session_state.email = receive.get('email')
@@ -17,14 +17,15 @@ def login():
 
 
 def test():
-    response = requests.post('https://www.boyastudio.top/test', json={'sid': sid, 'email': email, 'code': code})
-    st.session_state.chart = json.loads(response.text)
+    response = requests.post('http://127.0.0.1:5000/test', json={'sid': sid, 'email': email, 'code': code})
+    receive = response.json()
+    st.session_state.candlestick_options = receive.get('candlestick_options')
 
 
 if len(st.session_state.sid) == 0:
-    email = st.text_input('Email:')
-    password = st.text_input('Password:', type='password')
-    st.button('login', on_click=login)
+    email = st.text_input('邮箱：')
+    password = st.text_input('密码：', type='password')
+    st.button('登录', on_click=login)
 else:
     sid = st.session_state.sid
     email = st.session_state.email
@@ -34,6 +35,6 @@ else:
     with col1:
         code = st.text_input(label='Code:', label_visibility="collapsed")
     with col2:
-        st.button('test', on_click=test)
-    if st.session_state.chart is not None:
-        st_echarts(options=st.session_state.chart)
+        st.button('查询', on_click=test)
+    if st.session_state.candlestick_options is not None:
+        st_echarts(options=json.loads(st.session_state.candlestick_options))
